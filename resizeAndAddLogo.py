3a35ae485c15a353784c87da5d52959a0290c79b
SeanAbbott1 <sabbott2012@gmail.com>
2016-04-10 16:59:42 -0400

    SA Week 14 Sub 12

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -31,9 +31,9 @@ for filename in os.listdir('.'):
             width = int((SQUARE_FIT_SIZE / height) * width)
             height = SQUARE_FIT_SIZE
 
-    # Resize the image.
-    print('Resizing %s...' % (filename))
-    im = im.resize((width, height))
+            # Resize the image.
+            print('Resizing %s...' % (filename))
+            im = im.resize((width, height))
     
 #TODO: Add the logo.
        
