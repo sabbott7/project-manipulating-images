a246f96fe0fe47ef8e915f3430a1c514a3a658fb
SeanAbbott1 <sabbott2012@gmail.com>
2016-04-10 15:57:07 -0400

    SA Week 14 Sub 4

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -17,19 +17,19 @@ for filename in os.listdir('.'):
         continue    # skip non-image files and the logo file itself
     im = Image.open(filename)
     width, height = im.size
-    
+
 #TODO: Check if file image needs to be resized
 
 if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
    
 #TODO: Calculate the new width and height to resize to.
 
-if width > height:
-    height = int((SQUARE_FIT_SIZE / width) * height)
-    width = SQUARE_FIT_SIZE
-else:
-    width = int((SQUARE_FIT_SIZE / height) * width)
-    height = SQUARE_FIT_SIZE
+    if width > height:
+        height = int((SQUARE_FIT_SIZE / width) * height)
+        width = SQUARE_FIT_SIZE
+    else:
+        width = int((SQUARE_FIT_SIZE / height) * width)
+        height = SQUARE_FIT_SIZE
 
 # Resize the image.
 print('Resizing %s...' % (filename))
