19982c572282c79abd4a789ab0e9b2112670d274
SeanAbbott1 <sabbott2012@gmail.com>
2016-04-11 07:56:53 -0400

    SA Week 14 Sub 21

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -8,13 +8,12 @@ logoIm = Image.open( LOGO_FILENAME )
 logoWidth, logoHeight = logoIm.size
 
 os.makedirs('withLogo', exist_ok=True)
-
 #TODO: Loop over all files in the working directory
-
 for filename in os.listdir('.'):
     if not (filename.endswith('.png') or filename.endswith('.jpg')) \
        or filename == LOGO_FILENAME:
-        continue    # skip non-image files and the logo file itself
+        continue # skip non-image files and the logo file itself
+    
     im = Image.open(filename)
     width, height = im.size
 
