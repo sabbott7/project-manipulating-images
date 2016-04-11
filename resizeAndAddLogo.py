a5477149a58a4868c64da82087a44ab8d4b07698
SeanAbbott1 <sabbott2012@gmail.com>
2016-04-11 02:59:58 -0400

    SA Week 14 Sub 19

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 
 SQUARE_FIT_SIZE = 300
-LOGO_FILENAME = 'logo.png'
+LOGO_FILENAME = 'catlogo.png'
 
 logoIm = Image.open( LOGO_FILENAME )
 logoWidth, logoHeight = logoIm.size
